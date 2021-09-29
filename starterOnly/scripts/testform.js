0670ba693b03a486d37e15b9083f5a52af305bbc
nailuJ <78428426+nailuJ-dev@users.noreply.github.com>
2021-09-29 20:31:32 +0200

    check error error message

--- starterOnly/scripts/testform.js
+++ starterOnly/scripts/testform.js
@@ -6,21 +6,23 @@ const getValueId = (id) => document.getElementById(id).value;
 const getErrorMsg = (classError) => document.querySelector(classError);
 
 const inputObject = [
-  { input: document.getElementById('first'), functionTest: () => checkString(), error: document.querySelector('.firstnameErrorMessage'), extra: 2 },
-  { input: document.getElementById('last'), functionTest: () => checkString(), error: document.querySelector('.lastnameErrorMessage'), extra: 2 },
-  { input: document.getElementById('email'), functionTest: () => checkEmail(), error: document.querySelector('.emailErrorMessage'), extra: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ },
-  { input: document.getElementById('birthdate'), functionTest: () => checkBirthdate(), error: document.querySelector('.birthdateErrorMessage'), extra: /(19\d\d|20[0-3])(-\d\d){2}/ },
-  { input: document.getElementById('quantity'), functionTest: () => checkString(), error: document.querySelector('.quantityErrorMessage'), extra: 0 }, // Tournaments category
-  { input: document.querySelectorAll('#citychoices .checkbox-input'), functionTest: () => checkCities(), error: document.querySelector('.citiesErrorMessage') },
-  { input: document.getElementById('checkbox1'), functionTest: () => checkTerms(), error: document.querySelector('.termsErrorMessage') }]
+  { input: document.getElementById('first').value.trim().length, functionTest: () => checkString(), error: document.querySelector('.firstnameErrorMessage'), extra: 2 },
+  { input: document.getElementById('last').value.trim().length, functionTest: () => checkString(), error: document.querySelector('.lastnameErrorMessage'), extra: 2 },
+  { input: document.getElementById('email').value, functionTest: () => checkEmail(), error: document.querySelector('.emailErrorMessage'), extra: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ },
+  { input: document.getElementById('birthdate').value, functionTest: () => checkBirthdate(), error: document.querySelector('.birthdateErrorMessage'), extra: /(19\d\d|20[0-3])(-\d\d){2}/ },
+  { input: document.getElementById('quantity').value, functionTest: () => checkString(), error: document.querySelector('.quantityErrorMessage'), extra: 0 }, // Tournaments category
+  { input: document.querySelectorAll('#citychoices .checkbox-input').checked, functionTest: () => checkCities(), error: document.querySelector('.citiesErrorMessage') },
+  { input: document.getElementById('checkbox1').checked, functionTest: () => checkTerms(), error: document.querySelector('.termsErrorMessage') }]
+
 
 // function to check string length
 function checkString (input, error, extra) {
-  if (input.trim().length < extra) {
+  if (input > extra && input !== '') {
+    return true
+  } else {
     error.classList.remove('hidden');
     return false
   }
-  return true
 };
 
 // function to check email validity
@@ -62,7 +64,6 @@ function checkTerms (input, error) {
 
 // Checking if all inputs are valid
         // const integralFormValid = () => checkString() && checkEmail() && checkBirthdate() && checkCities() && checkTerms()
-// inputObject.forEach (if item etc.)
 
 /* let integralFormValid = false
 
@@ -81,15 +82,15 @@ inputObject.forEach(function (items) {
 // Events for submiting the form
 registerForm.addEventListener('submit', function (event) {
   event.preventDefault()
-  console.log('merde');
+  console.log('shit');
   // if integralFormValid is true
   inputObject.forEach(function (items) {
     if (items.functionTest(items.input, items.error, items.extra) === true) {
-      console.log('fais chier')
+      console.log('fuck it')
       showNotifValid();
       registerForm.reset();
     } else {
-      console.log('merdebis');
+      console.log('fuck off');
     }
   });
 });
