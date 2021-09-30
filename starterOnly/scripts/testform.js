2c8319408839b70dbe65411538c8740580e32df1
nailuJ <78428426+nailuJ-dev@users.noreply.github.com>
2021-09-30 21:41:48 +0200

    check function test

--- starterOnly/scripts/testform.js
+++ starterOnly/scripts/testform.js
@@ -1,99 +1,78 @@
 // DOM Elements
-const registerForm = document.querySelector('.registration-form');
-
-// Functions to check the validity of datas'form
-const getValueId = (id) => document.getElementById(id).value;
-const getErrorMsg = (classError) => document.querySelector(classError);
-
-const inputObject = [
-  { input: document.getElementById('first').value.trim().length, functionTest: () => checkString(), error: document.querySelector('.firstnameErrorMessage'), extra: 2 },
-  { input: document.getElementById('last').value.trim().length, functionTest: () => checkString(), error: document.querySelector('.lastnameErrorMessage'), extra: 2 },
-  { input: document.getElementById('email').value, functionTest: () => checkEmail(), error: document.querySelector('.emailErrorMessage'), extra: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ },
-  { input: document.getElementById('birthdate').value, functionTest: () => checkBirthdate(), error: document.querySelector('.birthdateErrorMessage'), extra: /(19\d\d|20[0-3])(-\d\d){2}/ },
-  { input: document.getElementById('quantity').value, functionTest: () => checkString(), error: document.querySelector('.quantityErrorMessage'), extra: 0 }, // Tournaments category
-  { input: document.querySelectorAll('#citychoices .checkbox-input').checked, functionTest: () => checkCities(), error: document.querySelector('.citiesErrorMessage') },
-  { input: document.getElementById('checkbox1').checked, functionTest: () => checkTerms(), error: document.querySelector('.termsErrorMessage') }]
-
+const registerForm = document.querySelector('.registration-form')
 
 // function to check string length
 function checkString (input, error, extra) {
-  if (input < extra && input === '') {
-    error.classList.remove('hidden');
+  if (input.length < extra) {
+    error.classList.remove('hidden')
     return false
   }
   return true
 };
 
 // function to check email validity
-function checkEmail (input, error, extra = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/) {
-  if (extra.test(input) === false) {
-    error.classList.remove('hidden');
-    return false
+function checkEmail (input, error, extra) {
+  if (input) {
+    if (extra.test(input)) {
+      return true
+    }
   }
-  return true
+  error.classList.remove('hidden')
+  return false
 };
 
 // function to check age
 function checkBirthdate (input, error, extra) {
-  if (extra.test(input) === false) {
-    error.classList.remove('hidden');
-    return false
+  if (input) {
+    if (extra.test(input)) {
+      return true
+    }
   }
-  return true
+  error.classList.remove('hidden')
+  return false
 };
 
 // function to check cities of tournaments
 function checkCities (input, error) {
-  for (let i = 0; i < input.length; i++) {
-    if (input[i].checked === false) {
-      error.classList.remove('hidden');
-      return false
-    }
-  }
-};
-
-// function to check terms and conditions
-function checkTerms (input, error) {
-  if (input.checked === false) {
-    error.classList.remove('hidden');
+  const res = [...input].filter(({ checked }) => checked.length)
+  if (!res) {
+    error.classList.remove('hidden')
     return false
   }
   return true
 };
 
-// Checking if all inputs are valid
-        // const integralFormValid = () => checkString() && checkEmail() && checkBirthdate() && checkCities() && checkTerms()
-
-/* let integralFormValid = false
-
-inputObject.forEach(function (items) {
-  if (items.functionTest === true) {
-    integralFormValid = true
-    console.log('fuck it')
-  } else {
-    console.log('fuck off');
+// function to check terms and conditions
+function checkTerms (input, error) {
+  if (input.checked) {
+    return true
   }
-});
-*/
-
-// Or const integralFormValid = Object.values(inputObject).filter(functiontest === true)
+  error.classList.remove('hidden')
+  return false
+};
 
 // Events for submiting the form
 registerForm.addEventListener('submit', function (event) {
   event.preventDefault()
-  console.log('shit');
-  // if integralFormValid is true
+  const inputObject = [
+    { input: document.getElementById('first').value, functionTest: checkString, error: document.querySelector('.firstnameErrorMessage'), extra: 2 },
+    { input: document.getElementById('last').value, functionTest: checkString, error: document.querySelector('.lastnameErrorMessage'), extra: 2 },
+    { input: document.getElementById('email').value, functionTest: checkEmail, error: document.querySelector('.emailErrorMessage'), extra: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ },
+    { input: document.getElementById('birthdate').value, functionTest: checkBirthdate, error: document.querySelector('.birthdateErrorMessage'), extra: /(19\d\d|20[0-3])(-\d\d){2}/ },
+    { input: document.getElementById('quantity').value, functionTest: checkString, error: document.querySelector('.quantityErrorMessage'), extra: 0 }, // Tournaments category
+    { input: document.querySelectorAll('#citychoices .checkbox-input'), functionTest: checkCities, error: document.querySelector('.citiesErrorMessage') },
+    { input: document.getElementById('checkbox1').checked, functionTest: checkTerms, error: document.querySelector('.termsErrorMessage') }
+  ]
+
+  let functionCount = 0
+
   inputObject.forEach(function (items) {
-    if (items.functionTest(items.input, items.error, items.extra) === true) {
-      console.log('fuck it')
-      let functionCount = 1;        // Count to avoid a validation submit with one lap for the each loop
-      const functionTotal = functionCount++;
-      if (functionTotal === 7) {
-        showNotifValid();
-        registerForm.reset();
+    if (items.functionTest(items.input, items.error, items.extra)) {
+      functionCount++
+      if (functionCount === inputObject.length) {
+        showNotifValid()
+        registerForm.reset()
       }
-    } else {
-      console.log('fuck off');
     }
-  });
-});
+  })
+})
